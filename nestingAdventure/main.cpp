6de31dadabb8c909ec4d207c483ebde60225307b
Abel Anand <Abelanand13@gmail.com>
2021-11-04 08:50:44 -0700

    hola

--- nestingAdventure/main.cpp
+++ nestingAdventure/main.cpp
@@ -1,14 +1,25 @@
-//
-//  main.cpp
-//  nestingAdventure
-//
-//  Created by Abel Anand on 11/1/21.
-//
-
 #include <iostream>
+#include <string>
+#include <iostream>
+#include <thread>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    int choice1;
+    bool quit;
+    cout << "It is the year 19 BBY, your master and you along with your clones, have defeated the droid army in Christophsis\n You are celebrating your victory when suddenly two clones come into your tent with their guns.\n\"Join the empire(1), or die(2)\"\nWhat do you do...\n";
+    cin >> choice1;
+    switch (choice1) {
+        case 1:
+            switch(choice1){
+                case 1:
+            }
+            break;
+        case 2:
+            break;
+        default:
+            
+            break;
+    }
+    
 }
