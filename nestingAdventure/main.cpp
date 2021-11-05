fbc79b3c41e7448c117d5405cf3466abb0b003e0
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-11-04 21:57:55 -0700

    commet

--- nestingAdventure/main.cpp
+++ nestingAdventure/main.cpp
@@ -3,22 +3,26 @@
 #include <iostream>
 #include <thread>
 using namespace std;
-
+void empire() {
+    cout << "Whats the empire\nHaven't you Heard, it's what's replacing the republic\nOk, I guess I'll join, how about you master.\nNo, I think I'd rather stay, he says as he tries igniting his lightsaber. Before his blade can reach it's extended form, the clones blast him.";
+    
+}
+void die() {
+    cout << "You and your master ignite your lightsabers an stand in a defence position\nWhat are you doing Commander, says your master.\nFollowing Orders\nThe clones fire at you, but you block it back easily\nYou and your master easily dispose of the clones. ";
+}
 int main() {
     int choice1;
     bool quit;
-    cout << "It is the year 19 BBY, your master and you along with your clones, have defeated the droid army in Christophsis\n You are celebrating your victory when suddenly two clones come into your tent with their guns.\n\"Join the empire(1), or die(2)\"\nWhat do you do...\n";
+    cout << "It is the year 19 BBY, your master and you along with your clones, have defeated the droid army in Christophsis\nYou are celebrating your victory when suddenly two clones come into your tent with their guns.\nThey say,\"Join the empire(1), or die(2)\"\nWhat do you do...\n";
     cin >> choice1;
     switch (choice1) {
         case 1:
-            switch(choice1){
-                case 1:
-            }
+            empire();
             break;
         case 2:
+            die();
             break;
         default:
-            
             break;
     }
     
