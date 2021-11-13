59f082cd028c6b2d9dca51ff9fc64e127815fc67
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-11-13 10:22:58 -0800

    commit4

--- nestingAdventure/main.cpp
+++ nestingAdventure/main.cpp
@@ -6,17 +6,12 @@
 #include <unistd.h>
 #include <time.h>
 using namespace std;
+
 void attack() {
     int choice1;
     bool quit = false;
     cout << "You and your master decide to attack the rest of the clones" << sleep(3) << "\nYou take a peek outside your tent to see a few clone approaching, they must also be coming to share the message" << sleep(3) << "\nShould you Go outside and Kill them(1), Sabotage them when they come inside(2), or hide(3)\n";
-    cin >> choice1;
-}
-void escape() {
-    int choice1;
-    bool quit = false;
-    cout << "You and your master decide to escape, you cut a hole in the back of the net and escape through there." << sleep(3) << "You have to leave the camp and luckily, you see two vehicles, a Ship(1) and a Speeder bike(2)" << sleep(3) << "Which one do you take\n";
-    cin >> choice1;
+
     while (!quit) {
        cin >> choice1;
    switch (choice1) {
@@ -32,21 +27,75 @@ void escape() {
            }
        }
 }
+
+void escape() {
+    int choice1;
+    bool quit = false;
+    cout << "You and your master decide to escape, you cut a hole in the back of the net and escape through there." << sleep(3) << "You have to leave the camp, luckily, you see two vehicles outside, a Ship(1) and a Speeder bike(2)" << sleep(3) << "Which one do you take\n";
+    
+    while (!quit) {
+       cin >> choice1;
+   switch (choice1) {
+       case 1:
+           quit = true;
+           break;
+       case 2:
+           quit = true;
+           break;
+       default:
+           cout << "Make a valid decision!\n";
+           break;
+           }
+       }
+}
+
 void avenge() {
     int choice1;
     bool quit = false;
-    cout << "\nNO MASTER!" <<"An instant reflex kicks in, you slice the clones and go over to your master." << sleep(3) << "Go, your master says. Be safe, May the force be wi- ";
-    cout << sleep(3) << "You could've prevented it" << sleep(2) << "eat grabage";
+    cout << "NO MASTER!" <<"\nAn instant reflex kicks in, you slice the clones and go over to your master." << sleep(3) << "\nGo, your master says. Be safe, May the force be wi- ";
+    cout << sleep(3) << "\nYou could've prevented it" << sleep(2) << "\nYou are filled with rage." << sleep(2) << "Should you use that rage to kill all the clones outside(1), or should you contain that rage and think about what you are going to do next(2)";
+    
+    while (!quit) {
+       cin >> choice1;
+   switch (choice1) {
+       case 1:
+           quit = true;
+           break;
+       case 2:
+           quit = true;
+           break;
+       default:
+           cout << "Make a valid decision!\n";
+           break;
+           }
+       }
 }
 void stick() {
     int choice1;
     bool quit = false;
+    
+    while (!quit) {
+       cin >> choice1;
+   switch (choice1) {
+       case 1:
+           quit = true;
+           break;
+       case 2:
+           quit = true;
+           break;
+       default:
+           cout << "Make a valid decision!\n";
+           break;
+           }
+       }
 }
+
 void empire() {
     int choice1;
     bool quit = false;
-    cout << "Whats the empire?" << sleep(3) <<"\nHaven't you Heard, it's what's replacing the republic" << sleep(3) << "\nOk, I guess I'll join, how about you master." << sleep(3) << "\nNo, I think I'd rather stay, he says as he tries igniting his lightsaber. Before his blade can reach it's extended form, the clones blast him.";
+    cout << "Whats the empire?" << sleep(3) <<"\nHaven't you Heard, it's what's replacing the republic" << sleep(3) << "\nOk, I guess I'll join, how about you master." << sleep(3) << "\nNo, I think I'd rather not, he says as he tries igniting his lightsaber. Before his blade can reach it's extended form, the clones blast him.";
     cout << sleep(3) << "\nDo you help your master(1) or Do you stay with your choice(2)\n";
+    
     while (!quit) {
     cin >> choice1;
 switch (choice1) {
@@ -64,14 +113,16 @@ switch (choice1) {
         }
     }
 }
+
 void die() {
     int choice1;
     bool quit = false;
     cout << "You and your master ignite your lightsabers an stand in a defence position" << sleep(3) <<"\nWhat are you doing Commander, says your master." << sleep(3) << "\nFollowing Orders" << sleep(3) << "\nThe clones fire at you, but you block it back easily" << sleep(3) << "\nYou and your master easily dispose of the clones.";
     cout << sleep(3) << "\nSince you are the only Jedi in the camp you are outnumbered and have two choices" << sleep(3) << "\nYou can attack all the clones(1), or escape somehow(2)\n" ;
+    
     while (!quit) {
     cin >> choice1;
-switch (choice1) {
+    switch (choice1) {
     case 1:
         attack();
         quit = true;
@@ -87,13 +138,13 @@ switch (choice1) {
     }
 }
 
-
 int main() {
     int choice1;
     bool quit = false;
         cout << "It is the year 19 BBY, your master and you along with your clones, have defeated the droid army in Christophsis" << sleep(3) << "\nYou are celebrating your victory when suddenly two clones come into your tent with their guns." << sleep(3);
         cout << "\nThey say,\"Join the empire(1), or die(2)\"" << sleep(3) << "\nWhat do you do...\n";
-        while (!quit) {
+        
+    while (!quit) {
         cin >> choice1;
     switch (choice1) {
         case 1:
